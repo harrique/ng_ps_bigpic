e9e8a4d41b2fb2072f5691ef1fa7f0ac8aaac848
harrique <harrique@gmial.com>
2018-12-09 01:39:38 +0300

    BP.3.1 - APM start

--- BigPic/src/app/app.module.ts
+++ BigPic/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
