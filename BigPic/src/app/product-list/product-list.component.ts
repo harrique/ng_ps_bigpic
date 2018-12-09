da47fe061ffc5d6c8373ce6e62787453cab9c7f5
Dmitriy Gorelov <GORELD@eu.ad.whirlpool.com>
2018-12-09 23:39:35 +0300

    BP.6.1 - APM product-list 2 - templates, directives

--- BigPic/src/app/product-list/product-list.component.ts
+++ BigPic/src/app/product-list/product-list.component.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product-list',
+  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  pageTitle: string;
+  products: Array<any>;
+
+  constructor() { 
+    this.pageTitle = 'Product List';    
+  }
 
   ngOnInit() {
+    this.products = [
+      {
+        "productId": 1,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2016",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+      },
+      {
+        "productId": 5,
+        "productName": "Hammer",
+        "productCode": "TBX-0048",
+        "releaseDate": "May 21, 2016",
+        "description": "Curved claw steel hammer",
+        "price": 8.9,
+        "starRating": 4.8,
+        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+      }
+    ];
   }
 }
